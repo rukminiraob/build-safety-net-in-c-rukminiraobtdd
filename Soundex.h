15a4d201687247fb19cb732a7f93a900a858cc1a
rukminiraob <braorukmini@gmail.com>
2024-09-17 22:41:19 +0530

    different delimiter

--- Soundex.h
+++ Soundex.h
@@ -6,7 +6,6 @@
 #include <string.h>
 #include <stdio.h>
 
-// Add function implementation
 int Add(const char *numbers) {
     if (numbers[0] == '\0') {
         return 0;  // Return 0 for an empty string
@@ -14,12 +13,21 @@ int Add(const char *numbers) {
 
     int sum = 0;
     const char *delimiter = ",";
-    char *token = strtok((char *)numbers, delimiter);
+    char *numbers_copy = strdup(numbers);  // Make a mutable copy of the input string
+
+    // Check for custom delimiter
+    if (numbers[0] == '/' && numbers[1] == '/') {
+        delimiter = (char[]){numbers[2], '\0'};  // Set the delimiter as the third character
+        numbers_copy = strchr(numbers, '\n') + 1;  // Skip past the first line
+    }
+
+    char *token = strtok(numbers_copy, delimiter);
     
     while (token != NULL) {
         sum += atoi(token);  // Convert string to int and add to sum
         token = strtok(NULL, delimiter);
     }
-    
+
+    free(numbers_copy);
     return sum;
 }
